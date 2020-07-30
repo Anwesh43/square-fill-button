963489f0e0e71bf02ea9dc942d5430aad9ca4aad
Anwesh43 <anweshthecool0@gmail.com>
2020-07-30 18:25:39 +0530

    adding initial project setup

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'square-fill-button'
+import 'square-fill-button/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
